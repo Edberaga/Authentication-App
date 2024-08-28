820284357c02f1b1ccdc79f1ac0bdd174af80664
edberaga <edbetjonnathan@gmail.com>
2024-08-28 16:41:45 +0700

    Develop the authentication program

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function authenticate() {
+    const name = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+    console.log(`name: ${name}, pass: ${pass}`);
+
+    if(name === "edbert" && pass === "1234") {
+        alert("Welcome back Edbert!");
+    } else {
+        alert("Invalid credentials. Please try again.");
+    }
+}
+
 function moveToRegister() {
     const register = document.getElementsByClassName('register');
     const login = document.getElementsByClassName('login');
